62ff12f568c7f0572f21da5c7826bcf9507530bc
Rajeev Karamcheti <rkaramc@gmail.com>
2026-04-19 01:48:58 +0530

    refactor(lsp): split crate into library and binary

---
+++ lsp/src/lib.rs
@@ -0,0 +1,10 @@
+pub mod coordinates;
+pub mod documents;
+pub mod evaluator;
+pub mod inlay_hints;
+pub mod server;
+
+pub use coordinates::LineIndex;
+pub use documents::{Document, DocumentStore};
+pub use evaluator::{Evaluator, EvalResult, RangeResult};
+pub use server::{Server, SharedState, EvalTask, EvalAction};
